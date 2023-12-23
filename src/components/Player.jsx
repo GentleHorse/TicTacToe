5279a87dadc07c8de5d9e6710ff789cb778ec5e2
GentleHorse <anch.abc.t@gmail.com>
2023-12-23 01:03:38 +0100

    Create the Player component and GameBoard component

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editNameHandler = () => {
+    setIsEditing((isEditing) => !isEditing);
+  };
+
+  const changeHandler = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={changeHandler} />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={editNameHandler}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
